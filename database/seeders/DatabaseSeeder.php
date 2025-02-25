f5a981daec107245fde7be5a919497ec30823a6d
deiker619 <deiker1842@gmail.com>
2025-02-25 11:39:37 -0400

    delete; user::factory seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Prueba Deiker',
             'email' => 'deiker1842@example.com',
             'password' => bcrypt('12345678')
-        ])->assignRole('Admin');
+        ])->assignRole('Analist');
     }
 }
