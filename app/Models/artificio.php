8d74011ebc009c8ab2af3dbc113a6c42a5f82d9c
deiker619 <deiker1842@gmail.com>
2025-11-13 12:03:57 -0400

    se corrigio el modelo

--- app/Models/artificio.php
+++ app/Models/artificio.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class artificio extends Model
+class Artificio extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'artificio'];
