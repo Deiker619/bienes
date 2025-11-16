cb56cb1e805d3fee7930c670fd7d5cb821a58073
Gerardo <torresanibal388@gmail.com>
2025-11-16 19:04:20 -0400

    se modifico el cintillo de reportes all blade

--- resources/views/livewire/retiros/pdf/retiros-all.blade.php
+++ resources/views/livewire/retiros/pdf/retiros-all.blade.php
@@ -7,6 +7,16 @@
             padding: 20px;
         }
 
+           .nota-title img {
+            width: 116%;
+            max-width: none;
+            height: auto;
+            display: block;
+            margin-left: -8%;
+            position: relative;
+            top: -30px;
+        }
+
         .table-container {
             width: 100%;
             margin: 0 auto;
@@ -48,6 +58,11 @@
             margin: 0;
         }
     </style>
+
+    
+    <div class="nota-title">
+        <img src="assets/images/cintillo2.png" alt="">
+    </div>
 
     <h1>Reporte de Retiro</h1>
 
