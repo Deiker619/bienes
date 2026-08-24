9946cf9e8c2642a32000bd41bf9b5a8ecc02f321
Gerardo8943 <torresanibal388@gmail.com>
2026-08-24 10:48:25 -0400

    Documentacion del archivo grafica retiro

--- app/Livewire/Dashboard/GraficaRetiro.php
+++ app/Livewire/Dashboard/GraficaRetiro.php
@@ -19,6 +19,7 @@ class GraficaRetiro extends Component
     public function render()
     {
         $chart = $this->chartRetirosMes();
+        //Aca hace el calculo del mes vacio
         $chart['vacio'] = count($chart['data']) === 0;
 
         return view('livewire.dashboard.grafica-retiro', compact('chart'));
